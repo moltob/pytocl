e6f8427185748fdd7a025930c29b03cb27c01a3a
ironhide <ironhide@hackathon>
2016-10-27 16:14:46 +0200

    brake ad accel

--- pytocl/driver.py
+++ pytocl/driver.py
@@ -18,6 +18,7 @@ class Driver:
     def __init__(self, logdata=True):
         self.data_logger = DataLogWriter() if logdata else None
         self.accelerator = 0.0
+        self.brake = 0.0
         self.pid_angle = PID(2.5, 0.01, 0.7)
         self.pid_dist = PID(2.5, 0.01, 0.7)
         self.pid_speed = PID(2.5, 0.01, 0.7)
@@ -55,19 +56,29 @@ class Driver:
         steering_stellgr_angle = (-self.pid_angle.control(carstate.angle, 0) / 180)
         steering_stellgr_dist = self.pid_dist.control(carstate.distance_from_center, 0)
 
-
-
         command.steering = (4*steering_stellgr_angle + steering_stellgr_dist) / 5
 
         # basic acceleration to target speed:
-        if carstate.speed_x < 40 * MPS_PER_KMH:
-            self.accelerator += 0.1
-        else:
-            self.accelerator = 0
-        self.accelerator = min(1, self.accelerator)
-        self.accelerator = max(-1, self.accelerator)
+        #if carstate.speed_x < 40 * MPS_PER_KMH:
+        #    self.accelerator += 0.1
+        #else:
+        #    self.accelerator = 0
+        #self.accelerator = min(1, self.accelerator)
+        #self.accelerator = max(-1, self.accelerator)
+
+
+        tar_speed = self.calc_target_speed(carstate)
+
+        speed_control = self.pid_speed.control(carstate.speed_x, tar_speed * MPS_PER_KMH)
+
+        _logger.info('speed_control: {}'.format(speed_control))
+
+        self.accel_and_brake(carstate.speed_x, tar_speed)
+
         command.accelerator = self.accelerator
         _logger.info('accelerator: {}'.format(command.accelerator))
+
+        command.brake = self.brake
 
         # gear shifting:
         _logger.info('rpm, gear: {}, {}'.format(carstate.rpm, carstate.gear))
@@ -87,3 +98,34 @@ class Driver:
         _logger.info("Distance: " + str(carstate.distance_from_start))
 
         return command
+
+    def accel_and_brake(self, cur_speed, target_speed):
+        if cur_speed < target_speed * MPS_PER_KMH:
+            self.accelerator += 0.1
+            self.brake = 0
+        elif (cur_speed - (target_speed * MPS_PER_KMH)) > 50:
+            self.brake += 0.1
+            self.accelerator = 0
+        elif cur_speed > target_speed * MPS_PER_KMH:
+            self.brake = 0.1
+            self.accelerator = 0
+        else:
+            self.accelerator = 0
+            self.brake = 0
+
+        self.accelerator = min(1, self.accelerator)
+        self.accelerator = max(-1, self.accelerator)
+        self.brake = min(1, self.brake)
+        self.brake = max(-1, self.brake)
+
+    def calc_target_speed(self, carstate: State):
+        if carstate.distances_from_edge[9] < 35:
+            return 55
+        if carstate.distances_from_edge[9] < 50:
+            return 80
+        if carstate.distances_from_edge[9] < 100:
+            return 100
+        if carstate.distances_from_edge[9] < 150:
+            return 150
+        else:
+            return 200
