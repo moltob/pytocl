7b1c0fb8877cfe7760756951f75da710e480e7cb
jetfire <jetfire@hackathon>
2016-10-27 16:20:08 +0200

    Merge branch 'master' of ssh://hackserv.fritz.box:/repo/jetfire
    # Conflicts:
    #	pytocl/driver.py

--- pytocl/acceleration.py
+++ pytocl/acceleration.py
@@ -1,11 +1,26 @@
 from pytocl.pid import PID
 from pytocl.car import State, MPS_PER_KMH
 
+class TrackParameter:
+    def __init__(self, start, velocity):
+        self.start = start
+        self.velocity = velocity
 
 class Acceleration:
     def __init__(self):
         self.control = PID(0.5, 0.1, 0.2, Integrator_max=20, Integrator_min=-20)
         self.targetVelocity = 21.6
+        self.targetTrackVelocity = [
+            TrackParameter(400, 15), TrackParameter(400+100, 30),
+            TrackParameter(2400, 15), TrackParameter(2500, 30),
+            TrackParameter(3200, 15), TrackParameter(3300, 30)]
+
+    def setTargetVelocity(self, carstate):
+        self.targetVelocity = 30
+        position = carstate.distance_from_start
+        for track in self.targetTrackVelocity:
+            if position >= track.start:
+                self.targetVelocity = track.velocity
 
     def update(self, carstate):
         # if carstate.speed_x < (50 * MPS_PER_KMH):
@@ -13,6 +28,7 @@ class Acceleration:
         # else:
         #    self.targetVelocity -= (10 * MPS_PER_KMH)
 
+        self.setTargetVelocity(carstate)
         deltaVelocity = (self.targetVelocity - (carstate.speed_x))
 
         return self.control.update(deltaVelocity)
