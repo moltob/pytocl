8d435b7888bd45abecda12fb3c7f40980ec75990
jetfire <jetfire@hackathon>
2016-10-27 18:40:59 +0200

    integrated acceleration

--- pytocl/acceleration.py
+++ pytocl/acceleration.py
@@ -4,14 +4,13 @@ from pytocl.car import State, MPS_PER_KMH
 
 class Acceleration:
     def __init__(self):
-        self.control = PID(0.5, 0.1, 0.2, Integrator_max=20, Integrator_min=-20)
+        self.control = PID(0.6, 0.1, 0.6, Integrator_max=20, Integrator_min=-20)
         self.targetVelocity = 20.0
 
     def update(self, carstate):
-        # if carstate.speed_x < (50 * MPS_PER_KMH):
-        #    self.targetVelocity += (10 * MPS_PER_KMH)
-        # else:
-        #    self.targetVelocity -= (10 * MPS_PER_KMH)
+        deltaAngle = max(min(1, (carstate.angle / 10.0) - (carstate.distance_from_center * 0.4)), -1)
+
+        self.targetVelocity = max(20, ((1.0 - abs(deltaAngle)) * 30.0))
 
         deltaVelocity = (self.targetVelocity - (carstate.speed_x))
 
