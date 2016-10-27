5d3913990350888a038b28393e17d8a715eafac6
jetfire <jetfire@hackathon>
2016-10-28 01:43:47 +0200

    Merge branch 'master' of ssh://hackserv.fritz.box:/repo/jetfire
    # Conflicts:
    #	pytocl/driver.py

--- logfiles/logoutput.py
+++ logfiles/logoutput.py
@@ -1,14 +1,22 @@
 from pytocl.analysis import DataLogReader
+from matplotlib import pyplot as plt
 
 data = DataLogReader(
-    '../pytocl/drivelogs/drivelog-2016-10-27-15-27-46.pickle',
+    '../pytocl/drivelogs/drivelog-2016-10-28-00-24-54.pickle',
     # state_attributes=('angle', 'focused_distances_from_edge'),
-    state_attributes=('distances_from_edge','opponents'),
-     command_attributes=()
+    state_attributes=('angle', 'distances_from_edge'),
+    command_attributes=('accelerator', 'brake')
 ).array
 
-for zeile in data:
-    print(zeile[0],zeile[1])
+print(len(data[0]))
 
-print(data[0])
+plt.plot(data[:,0], data[:,21])
+plt.xlabel('time')
+plt.ylabel('angle')
+
+#for cycle in data:
+#    time = cycle[0]
+#    angle = cycle[1]
+#    accelerator = cycle[20]
+#    brake = cycle[21]
 
