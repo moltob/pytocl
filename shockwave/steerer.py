c61313505ed60a74f031f76ac147ef3534f14717
Team Shockwave <shockwave>
2016-10-27 15:02:42 +0200

    Steerer adjusted

--- shockwave/steerer.py
+++ shockwave/steerer.py
@@ -1,20 +1,22 @@
 from pytocl.car import State
-from shockwave.plan import Plan
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class Steerer:
     def __init__(self, plan):
         self.plan = plan
-        self.pid = SteeringPID(0.5, 0.01, 0.001)
-        self.last_angle = 0
+        self.pid = SteeringPID(0.25, 0, 0)
+        self.angle = 0
 
     def get_steering_angle(self, state: State):
-        self.last_angle += self.pid.get_delta_steering(self.plan.get_desired_position() - state.distance_from_center)
-        if self.last_angle > 1:
-            self.last_angle = 1
-        elif self.last_angle < -1:
-            self.last_angle = -1
-        return self.last_angle
+        self.angle = self.pid.get_delta_steering(self.plan.get_desired_position() - state.distance_from_center)
+        if self.angle > 1:
+            self.angle = 1
+        elif self.angle < -1:
+            self.angle = -1
+        return self.angle
 
 
 class SteeringPID:
@@ -24,15 +26,18 @@ class SteeringPID:
         self.last_es = []
 
     def get_delta_steering(self, e):
+        _logger.info("Steering error %f", e)
         if self.last_e is None:
             self.last_e = e
 
         self.last_es.append(e)
         if len(self.last_es) > 20:
             self.last_es.pop(0)
+
         de = e - self.last_e
         ie = sum(self.last_es)
 
+        self.last_e = e
         out = self.kp*e + self.kd*de + self.ki*ie
 
         return out
