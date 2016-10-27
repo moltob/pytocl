4c7d9e8a351f734b62df99ce37cb9e1b0524d154
jazz <jazz@hackathon>
2016-10-27 14:36:28 +0200

    ToGe: Extended coordinate.

--- pytocl/jazzDriver/Coordinate.py
+++ pytocl/jazzDriver/Coordinate.py
@@ -1,4 +1,12 @@
 class Coordinate():
-    def __init__(self):
+    def __init__(self, distance, angle):
         self.distance = 0
         self.angle = 0
+
+    @property
+    def distance(self):
+        return self.distance
+
+    @property
+    def angle(self):
+        return self.angle
