d3e8030fcaaa3c242abd34ed3c583df8ef2e8b98
blackout <blackout@hackathon>
2016-10-27 16:24:55 +0200

    ver1

--- pytocl/lane.py
+++ pytocl/lane.py
@@ -33,7 +33,7 @@ class Lane:
                 max_dist = carstate.distances_from_edge[i]
                 max_idx = i
 
-        self.vel = max_dist * 0.75
+        self.vel = max_dist * 0.7
         self.ang = angles[max_idx]
 
 
