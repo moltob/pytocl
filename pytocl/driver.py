733a7e8b675986ed6b827f057249ee49e6d8c627
blackout <blackout@hackathon>
2016-10-27 14:05:11 +0200

    init

--- pytocl/driver.py
+++ pytocl/driver.py
@@ -17,6 +17,7 @@ class Driver:
     def __init__(self, logdata=True):
         self.data_logger = DataLogWriter() if logdata else None
         self.accelerator = 0.0
+        self.dynamic = Dynamic()
 
     @property
     def range_finder_angles(self):
@@ -47,28 +48,10 @@ class Driver:
         """
         command = Command()
 
-        # dummy steering control:
-        command.steering = (carstate.angle - carstate.distance_from_center * 0.5)
-
-        # basic acceleration to target speed:
-        if carstate.speed_x < 50 * MPS_PER_KMH:
-            self.accelerator += 0.1
-        else:
-            self.accelerator = 0
-        self.accelerator = min(1, self.accelerator)
-        self.accelerator = max(-1, self.accelerator)
-        command.accelerator = self.accelerator
-        _logger.info('accelerator: {}'.format(command.accelerator))
-
-        # gear shifting:
-        _logger.info('rpm, gear: {}, {}'.format(carstate.rpm, carstate.gear))
-        command.gear = carstate.gear or 1
-        if carstate.rpm > 7000 and carstate.gear < 6:
-            _logger.info('switching up')
-            command.gear = carstate.gear + 1
-        elif carstate.rpm < 2000 and carstate.gear > 1:
-            _logger.info('switching down')
-            command.gear = carstate.gear - 1
+        command.steering = dynamic.speed
+        command.accelerator = dynamic.acceleration
+        command.brake = dynamic.brake
+        command.gear = dynamic.gear
 
         if self.data_logger:
             self.data_logger.log(carstate, command)
---
+++ pytocl/lane.py
@@ -0,0 +1,13 @@
+class lane:
+
+
+    def __init__ (self):
+        self.velocity = 40
+        pass
+
+
+    def velocity(self):
+        return self.velocity
+
+    def angle(self):
+        return 0
