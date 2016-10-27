2e961d09264da7cae724df106643c94990ea83c4
bumblebee <bumblebee@hackathon>
2016-10-27 20:57:12 +0200

    bestRound

--- pytocl/strategy.py
+++ pytocl/strategy.py
@@ -24,52 +24,54 @@ class StrategyController:
         return self.speed, self.target_pos
 
     def control_speed(self, carstate: State):
-        cshape = self.detect_curve(carstate, 95)
+        cshape = self.detect_curve(carstate, 90)
         lane = self.detect_curve_lane(carstate)
         m = carstate.distances_from_edge
-        _logger.info('dist: {}, CURVE: {} -- LANE: {}||| {} || {} || {}'.format(carstate.distance_from_start, cshape, lane, m[8], m[9], m[10]))
+        #_logger.info('dist: {}, CURVE: {} -- LANE: {}||| {} || {} || {}'.format(carstate.distance_from_start, cshape, lane, m[8], m[9], m[10]))
         if cshape == 0:
             return 400, lane
         else:
             return 600 - 550*abs(cshape), lane
 
     def detect_curve_lane(self, carstate):
-        cshape = self.detect_curve(carstate, 40)
+        cshape = self.detect_curve(carstate, 27)
         if cshape != 0:
             if cshape < 0:
-                cshape = 2.0
+                cshape = 0.9
             else:
-                cshape = -2.0
-        else:
+                cshape = -1.06
+        """else:
             cshape = self.detect_curve(carstate, 180)
             if cshape > 0:
-                cshape = 0.0
+                cshape = 0
             else:
-                cshape = -0.0
+                cshape = -0"""
         return cshape
-
 
     def detect_curve(self, carstate: State, det_dist):
         m = carstate.distances_from_edge
-        cshape = abs(m[8] - m[10])
+        left = 8
+        middle = 9
+        right = 10
+
+        """if carstate.angle > 5:
+            offset = -1
+        elif carstate.angle < -5:
+            offset = 1
+        else:"""
+
+        offset = 0
+
+        cshape = abs(m[left+offset] - m[right-offset])
         if cshape > 100:
             cshape = 100
         cshape /= 100
         if carstate.distances_from_egde_valid and m[9] < det_dist:
-            if m[8] < m[9] < m[10]:
+            if m[left+offset] < m[middle+offset] < m[right-offset]:
                 return 1 - cshape
-            elif m[8] > m[9] > m[10]:
+            elif m[left+offset] > m[middle+offset] > m[right-offset]:
                 return -1 * (1 - cshape)
             else:
                 return 0
         else:
             return 0
-
-    def emergency_break(self, carstate: State):
-        m = carstate.distances_from_edge
-        if carstate.distances_from_egde_valid and m[9] < 20:
-            return True
-        else:
-            return False
-
-
