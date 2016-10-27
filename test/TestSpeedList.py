67bd70a30c4501137aa1c5cf7afa9a466accfb22
ironhide <ironhide@hackathon>
2016-10-27 16:11:06 +0200

    FN: actual implementation of speedlist

--- test/TestSpeedList.py
+++ test/TestSpeedList.py
@@ -3,12 +3,16 @@ from pytocl.speedlist import SpeedList
 def test_1():
     speedlist = SpeedList()
     speedlist.add(0, 0)
-    speedlist.add(1, 10)
-    speedlist.add(2, 20)
-    speedlist.add(3, 30)
-    speedlist.add(4, 40)
-    speedlist.add(5, 50)
-    speedlist.printAll()
+    speedlist.add(10, 10)
+    speedlist.add(20, 20)
+    speedlist.add(30, 30)
+    speedlist.add(40, 40)
+    speedlist.add(50, 50)
+    print(speedlist.getSpeedForDistance(30))
+    print(speedlist.getSpeedForDistance(35))
+    print(speedlist.getSpeedForDistance(45))
+    print(speedlist.getSpeedForDistance(60))
+    #speedlist.printAll()
 
 if __name__ == '__main__':
     test_1()
