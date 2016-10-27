11c548f343afbd235e3f59026357840cc9fcc8ee
Team Shockwave <shockwave>
2016-10-27 14:17:22 +0200

    added Gearer

--- shockwave/gearer.py
+++ shockwave/gearer.py
@@ -2,8 +2,16 @@ from pytocl.car import State
 
 
 class Gearer:
-    def __init__(self, plan):
-        self.plan = plan
 
+    @staticmethod
     def get_gear(self, state: State):
-        return 2 # -1 to 6
+        gear = state.gear or 1
+        if state.rpm > 9000 and state.gear < 5:
+            gear = state.gear + 2
+        elif state.rpm > 7000 and state.gear < 6:
+            gear = state.gear + 1
+        elif state.rpm < 1000 and state.gear > 2:
+            gear = state.gear - 2
+        elif state.rpm < 2000 and state.gear > 1:
+            gear = state.gear - 1
+        return gear
