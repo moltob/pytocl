c849cc446c2cca8fac19ca4b7375bda2690a9883
sideswipe <sideswipe@hackathon>
2016-10-27 17:52:09 +0200

    log reader

--- pytocl/log reader.py
+++ pytocl/log reader.py
@@ -1,19 +1,33 @@
 from pytocl.analysis import DataLogReader
 from matplotlib import pyplot  as plt
+from pytocl.log_utils import *
 
+drivelog = get_newest_drivelog()
+data = DataLogReader(drivelog,state_attributes=( 'angle','distance_from_center', 'distance_from_start', 'focused_distances_from_edge'),command_attributes=('steering',)).array
 
-data = DataLogReader('drivelogs/drivelog-2016-10-27-11-52-02.pickle',state_attributes=( 'angle', 'focused_distances_from_edge'),command_attributes=('steering',)).array
+print(drivelog)
 
+#print(data)
+print(data[0])
+print(data[1])
+print(data[0][0])
+print(data[1][0])
+print(data[:, 0])
 
-print(data[100][0])
+#plt.plot(data[:, 0],data[:, 1])
 
-#plt.plot(data)
 #plt.show()
 
 fig = plt.figure(figsize=(5, 2))  # initialize figure
-ax = fig.add_subplot(3, 2, 2)
+angle = fig.add_subplot(1,2,1)
 #ax.plot(x,y, '-o', c='red', lw=2, label='bla')  # plots a line
-ax.plot(data[:][0], data[:][1] , '-o', c='red', label='angle')
+angle.plot(data[:, 0],data[:, 1] , c='red', label='angle')
+#distance = fig.add_subplot(1,2,2)
+#distance.plot(data[:, 0],data[:, 2] , c='green', label='angle')
+
+startDistance = fig.add_subplot(1,2,2)
+startDistance.plot(data[:, 0],data[:, 3] , c='green', label='angle')
+#angle.plot(data[:, 0],data[:, 3] , c='green', label='angle')
 
 fig.show()
 plt.show()
