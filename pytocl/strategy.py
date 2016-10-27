e0b351ae6c03a38eaf00e5a1f916ea4aa12c4032
bumblebee <bumblebee@hackathon>
2016-10-27 15:37:34 +0200

    Integrated StrategyController into Driver

--- pytocl/strategy.py
+++ pytocl/strategy.py
@@ -15,11 +15,20 @@ class Curve(Enum):
 
 class StrategyController:
     def __init__(self):
-        pass
+        self.speed = 0
+        self.target_pos = 0
 
     def control(self, carstate: State):
+        self.speed = self.control_speed(carstate)
+
+        return self.speed, self.target_pos
+
+    def control_speed(self, carstate: State):
         curve = self.detect_curve(carstate)
-        return ()
+        if curve is Curve.NONE:
+            return 400
+        else:
+            return 60
 
     def detect_curve(self, carstate: State):
         m = carstate.distances_from_edge
