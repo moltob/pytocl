cc0006e7164b70ad41c94995dd5e5ecb5d8bb64b
blackout <blackout@hackathon>
2016-10-27 16:03:00 +0200

    ver1

--- pytocl/lane.py
+++ pytocl/lane.py
@@ -1,11 +1,24 @@
+from pytocl.analysis import DataLogWriter
+
+angles = [-90, -75, -60, -45, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 30, 45, 60, 75, 90]
+
 class Lane:
 
-
-
-
     def __init__ (self):
-        self.velocity = 120
-        pass
+        self.velocity = 0
+        self.angle = -1
+        self.data_logger = DataLogWriter()
+
+
+    @property
+    def range_finder_angles(self):
+        """Iterable of 19 fixed range finder directions [deg].
+
+        The values are used once at startup of the client to set the directions of range finders.
+        During regular execution, a 19-valued vector of track distances in these directions is
+        returned in ``state.State.tracks``.
+        """
+        return -90, -75, -60, -45, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 30, 45, 60, 75, 90
 
     def lookfwd(self, carstate):
         max_dist = 0
@@ -15,15 +28,18 @@ class Lane:
                 max_dist = carstate.distances_from_edge[i]
                 max_idx = i
 
+        self.velocity = max_dist * 0,75
+        self.angle = angles[max_idx]
 
 
-
+        print('max : {}, {}'.format(max_idx, max_dist))
+        print('distance_from_edge: {}'.format(carstate.distances_from_edge))
 
     def velocity(self):
         return self.velocity
 
     def angle(self):
-        return 0
+        return self.angle
 
 
 #_logger.info('distance_from_center: {}'.format(carstate.distance_from_center))
