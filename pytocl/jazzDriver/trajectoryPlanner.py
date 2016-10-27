9e624adce694848f2c5942483f937e1fbc69453e
jazz <jazz@hackathon>
2016-10-27 22:07:30 +0200

    ToGe: Added first target calculation.

--- pytocl/jazzDriver/trajectoryPlanner.py
+++ pytocl/jazzDriver/trajectoryPlanner.py
@@ -5,6 +5,7 @@ import math
 class TrajectoryPlanner:
     def __init__(self):
         self.rangeFinderAngles = [-90, -75, -60, -45, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 30, 45, 60, 75, 90]
+        self.anglesAverage = [0]*1
     @property
     def range_finder_angles(self):
         """Iterable of 19 fixed range finder directions [deg].
@@ -30,11 +31,11 @@ class TrajectoryPlanner:
         rightTrackBorderX = 0
         rightTrackBorderY = 0
         for x, y in zip(trackLimitCoordinatesX, trackLimitCoordinatesY):
-            if x > leftTrackBorderX and y > 0:
+            if x > leftTrackBorderX and y >= 0:
                 leftTrackBorderX = x
                 leftTrackBorderY = y
 
-            if x > leftTrackBorderX and y < 0:
+            if x > leftTrackBorderX and y <= 0:
                 rightTrackBorderX = x
                 rightTrackBorderY = y
 
@@ -48,6 +49,12 @@ class TrajectoryPlanner:
         else:
             angle = 0
 
+        nullIndex = 9
+        assert self.rangeFinderAngles[nullIndex] == 0
+        #if carstate.distances_from_edge[nullIndex] > 50:
+        #    angle =0
+        #    distance = carstate.distances_from_edge[nullIndex]
+
         #print(carstate.distances_from_edge)
         #print(trackLimitCoordinatesX)
         #print(trackLimitCoordinatesY)
@@ -55,4 +62,9 @@ class TrajectoryPlanner:
         #print(targetX, targetY)
         print(distance, angle)
 
-        return Coordinate(distance, angle)
+        self.anglesAverage.pop(0)
+        self.anglesAverage.append(angle)
+        averageAngle = sum(self.anglesAverage)/len(self.anglesAverage)
+        print(angle, averageAngle)
+
+        return Coordinate(distance, averageAngle)
