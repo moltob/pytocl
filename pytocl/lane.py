33ebf263969b6ed51d05189d45b5b7db6fff038e
blackout <blackout@hackathon>
2016-10-27 15:36:05 +0200

    ver1

--- pytocl/lane.py
+++ pytocl/lane.py
@@ -1,9 +1,22 @@
-class lane:
+class Lane:
+
+
 
 
     def __init__ (self):
-        self.velocity = 40
+        self.velocity = 120
         pass
+
+    def lookfwd(self, carstate):
+        max_dist = 0
+        max_idx = -1
+        for i in range(0,19):
+            if carstate.distances_from_edge[i] > max_dist:
+                max_dist = carstate.distances_from_edge[i]
+                max_idx = i
+
+
+
 
 
     def velocity(self):
@@ -11,3 +24,8 @@ class lane:
 
     def angle(self):
         return 0
+
+
+#_logger.info('distance_from_center: {}'.format(carstate.distance_from_center))
+#_logger.info('distance_from_edge: {}'.format(carstate.distances_from_edge))
+#_logger.info('angle: {}'.format(carstate.angle))
