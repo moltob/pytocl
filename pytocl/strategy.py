b8d23dbef8c697db67260d83782e3b4e2c5cfe1e
bumblebee <bumblebee@hackathon>
2016-10-27 15:39:21 +0200

    emergency break

--- pytocl/strategy.py
+++ pytocl/strategy.py
@@ -23,7 +23,7 @@ class StrategyController:
 
     def detect_curve(self, carstate: State):
         m = carstate.distances_from_edge
-        if carstate.distances_from_egde_valid and m[9] > 0 and m[9] < 150:
+        if carstate.distances_from_egde_valid and m[9] < 150:
             if m[8] < m[9] < m[10]:
                 return Curve.RIGHT
             elif m[8] > m[9] > m[10]:
@@ -32,3 +32,12 @@ class StrategyController:
                 return Curve.NONE
         else:
             return Curve.NONE
+
+    def emergency_break(self, carstate: State):
+        m = carstate.distances_from_edge
+        if carstate.distances_from_egde_valid and m[9] < 20:
+            return True
+        else:
+            return False
+
+
