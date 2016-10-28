d96cdae5c7d4645e87e14291ddc6050652924928
ironhide <ironhide@hackathon>
2016-10-28 11:52:10 +0200

    opponents

--- pytocl/opponents.py
+++ pytocl/opponents.py
@@ -1,5 +1,6 @@
 from pytocl.car import State
 from enum import Enum
+import numpy as np
 
 
 class Area(Enum):
@@ -10,7 +11,7 @@ class Area(Enum):
     RIGHT = (21, 31)
     REAR_LEFT = (1, 3)
     REAR_RIGHT = (32, 34)
-    REAR = (35, -1)
+    REAR = (35, 0)
 
     def __init__(self, begin, end):
         self.begin = begin
@@ -19,20 +20,26 @@ class Area(Enum):
 
 class Opponents:
     def __init__(self):
-        self.opponents_diff = []
-        self.opponents = []
+        self.opponents_diff = np.array([])
+        self.opponents = np.array([])
 
     def update(self, carstate: State):
         for i in range(0, 36):
             self.opponents_diff = carstate.opponents[i] - self.opponents[i]
 
-        self.opponents = carstate.opponents[:]
+        self.opponents = np.array(carstate.opponents)
 
     def dist_to_car(self, area: Area):
-        return min(self.opponents[area.begin:area.end])
+        if area == Area.REAR:
+            return min(self.opponents[[area.begin,area.end]])
+        else:
+            return min(self.opponents[area.begin:area.end])
 
     def diff_to_car(self, area: Area):
-        return min(self.opponents_diff[area.begin:area.end])
+        if area == Area.REAR:
+            return min(self.opponents_diff[[area.begin,area.end]])
+        else:
+            return min(self.opponents_diff[area.begin:area.end])
 
 
 
